ff756311cd67fd281cdb4f9a13cc0d790114415d
mostafa8722 <mostafa.8722@gmail.com>
2022-06-24 11:12:51 +0430

    sw3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,9 +27,9 @@ export default {
 
 
 
+
   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
