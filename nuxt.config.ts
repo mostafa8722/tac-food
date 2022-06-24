a448776c6e5e443fa61f0cbcf6cea72a7af3605d
mostafa8722 <mostafa.8722@gmail.com>
2022-06-24 11:23:50 +0430

    chnage_sw3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default {
     
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
+ 
   modules: [
     ['nuxt-leaflet', { ssr: false /* module options */ }],
     '@nuxtjs/toast',
