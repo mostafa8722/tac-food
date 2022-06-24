afdee10005eae1ee2f2f14d4f00df201f068e1f9
Mostafa Nazarband <34749049+mostafa8722@users.noreply.github.com>
2022-06-24 11:49:49 +0430

    Create sw.client.ts

---
+++ plugins/sw.client.ts
@@ -0,0 +1,23 @@
+import { Store } from "vuex/types/index";
+
+if ('serviceWorker' in navigator) {
+    console.log('Service worker:', 1)
+    navigator.serviceWorker.register('/custom-sw.js').then((registrations:any) => {
+       // console.log('Service worker:', registrations)
+      for (const worker of registrations) {
+        console.log('Service worker:', worker)
+      }
+    });
+  }else {
+    console.log('Service worker is not working')
+  }
+  let that = this;
+  window.addEventListener("beforeinstallprompt",e =>{
+    console.log('fetch data2');
+    e.preventDefault();
+    console.log('fetch data2',this);
+    //document.context.$store.dispatch('general/addInstallPromptEvent',e)
+    console.log('fetch data2');
+  })
+  
+  
